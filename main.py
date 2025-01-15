4847f916751ebbaea4370e25fd863c6b00416324
aavadhan10 <166965425+aavadhan10@users.noreply.github.com>
2025-01-15 08:49:30 -0800

    Update main.py

--- main.py
+++ main.py
@@ -524,18 +524,11 @@ def main():
     Please note, some skills listed may be industry specific or with a specialization. If not applicable, you can leave blank or put 0.
     
     ### Points Allocation Overview
-    You have **90 points** to allocate across **168 skills** listed in the matrix. These points represent your level of expertise 
+    You have **90 points** to allocate across **172 skills** listed in the matrix. These points represent your level of expertise 
     and experience in each area. The goal is to allocate your points in a way that best reflects your true areas of strength. 
     You'll need to make thoughtful choices about where your expertise lies, prioritizing key skills over areas of limited 
     experience to ensure we capture an honest reflection of your abilities.
-
-    ###Points Guidelines:
-    Maximum Points Per Skill: You can allocate a maximum of 10 points to any single skill.
-    Primary Areas of Expertise: You should allocate higher points (e.g., 8-10) to the skills where you have deep expertise or significant experience.
-    Secondary Areas of Expertise: For skills where you have some experience or intermediate-level knowledge, allocate moderate points (e.g., 3-7).
-    Limited Experience: For skills where you have limited experience or basic understanding, allocate fewer points (e.g., 1-2), or none at all.
-
-
+    
     ### How to Complete the Matrix
     
     **1. Review the Skills**
