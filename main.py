8e87b35a0d1ee746dbec1ad8945501e862fd6fd8
mngoyal <mgoyal@brieflylegal.com>
2025-01-22 10:00:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -673,7 +673,8 @@ def show_skills_form(submitter_email, submitter_name):
         return
 
     st.markdown("<u>**You can type a number directly or use the up/down arrows to enter your points.**</u>", unsafe_allow_html=True)
-    
+    st.markdown("<u>**Enter numbers slowly to allow the software time to register.**</u>", unsafe_allow_html=True)
+
     if st.session_state.total_points >= MAX_TOTAL_POINTS: 
         st.warning("You have used all 90 points. To add points to other skills, first reduce points elsewhere.")
     
